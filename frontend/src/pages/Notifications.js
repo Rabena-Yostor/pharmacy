fa958c505b4180d9e346b7784bec3a92c97318a7
PeterAshraf16 <peter_ashraf_2002@hotmail.com>
2023-12-05 22:34:22 +0200

    Update Notifications.js

--- frontend/src/pages/Notifications.js
+++ frontend/src/pages/Notifications.js
@@ -7,32 +7,49 @@ const NotificationsPage = () => {
   const [loading, setLoading] = useState(true);
   const username = localStorage.getItem('username');
 
-  useEffect(() => {
-    const fetchNotifications = async () => {
-      try {
-        const response = await fetch('api/medicine/getAllNotifications', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username }),
-        });
+  const fetchNotifications = async () => {
+    try {
+      const response = await fetch('api/medicine/getAllNotifications', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username }),
+      });
 
-        if (response.ok) {
-          const data = await response.json();
-          setNotifications(data.notifications);
-        } else {
-          console.error('Error:', response.statusText);
-          // Handle the error, show an error message on the page, etc.
-        }
-      } catch (error) {
-        console.error('Error:', error.message);
-        // Handle network errors or other exceptions
-      } finally {
-        setLoading(false);
+      if (response.ok) {
+        const data = await response.json();
+        setNotifications(data.notifications);
+      } else {
+        console.error('Error:', response.statusText);
       }
-    };
+    } catch (error) {
+      console.error('Error:', error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  const deleteNotification = async (notificationId) => {
+    try {
+      const response = await fetch(`api/medicine/deleteNotification/${notificationId}`, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        // Notification deleted successfully, update the state to reflect the change
+        setNotifications((prevNotifications) =>
+          prevNotifications.filter((notification) => notification._id !== notificationId)
+        );
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  useEffect(() => {
     fetchNotifications();
   }, []);
 
@@ -41,12 +58,16 @@ const NotificationsPage = () => {
       <h1>Notifications</h1>
       {loading ? (
         <p>Loading...</p>
+      ) : notifications.length === 0 ? (
+        <p>You don't have any notifications</p>
       ) : (
         <div>
           {notifications.map((notification) => (
             <div key={notification._id} style={notificationContainerStyle}>
-              <p>Notification : {notification.message}</p>
-              <p>Created At : {notification.timestamp}</p>
+              <p>Notification: {notification.message}</p>
+              <p>Status: Unread</p>
+              <p>Created At: {notification.timestamp}</p>
+              <button onClick={() => deleteNotification(notification._id)}>Mark as read and Delete</button>
             </div>
           ))}
         </div>
